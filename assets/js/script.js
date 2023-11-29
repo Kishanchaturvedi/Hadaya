81818d9d32721064d65765fe1f5c1e1a19c89065
anurag4499 <kisan12chaturvedi@gmail.com>
2023-11-29 19:28:37 +0530

    five

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,14 @@ function closeBtn() {
 
 
 
+
+
+
+
+
+
+
+
 $('#carouselCard-2').owlCarousel({
     loop: true,
 
@@ -162,6 +170,113 @@ $('#owls').owlCarousel({
     }
 })
 
+//********************* */ plp ***************************************************
+
+
+$('#paginationContainer').owlCarousel({
+    autoWidth: true,
+    margin: 15,
+    loop: false,
+    dots: false,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+var acc = document.getElementsByClassName("accordions");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("actives");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
+// range********************
+window.onload = function () {
+    slideOne();
+    slideTwo();
+}
+
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 0;
+let sliderTrack = document.querySelector(".slider-track");
+let sliderMaxValue = document.getElementById("slider-1").max;
+
+function slideOne() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+        sliderOne.value = parseInt(sliderTwo.value) - minGap;
+    }
+    displayValOne.textContent = sliderOne.value;
+    fillColor();
+}
+function slideTwo() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+        sliderTwo.value = parseInt(sliderOne.value) + minGap;
+    }
+    displayValTwo.textContent = sliderTwo.value;
+    fillColor();
+}
+function fillColor() {
+    percent1 = (sliderOne.value / sliderMaxValue) * 100;
+    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #AA8117 ${percent1}% , #AA8117 ${percent2}%, #dadae5 ${percent2}%)`;
+}
+
+
+
+// pagination 
+function toggleActive(link) {
+    var links = document.querySelectorAll('.nav-link.pagination a');
+    links.forEach(function (el) {
+        el.classList.remove('pagination-active');
+    });
+    link.classList.add('pagination-active');
+}
+
+var currentPage = 1;
+var totalPages = 7; // Total number of pages
+
+document.querySelector('.next-btn').addEventListener('click', function () {
+    if (currentPage < totalPages) {
+        currentPage++;
+        updatePagination(currentPage);
+    }
+});
+
+document.querySelector('.pre-btn').addEventListener('click', function () {
+    if (currentPage > 1) {
+        currentPage--;
+        updatePagination(currentPage);
+    }
+});
+
+function updatePagination(currentPage) {
+    var activeLink = document.querySelector('.pagination-active');
+    activeLink.classList.remove('pagination-active');
+    var links = document.querySelectorAll('.nav-link.pagination a');
+    links[currentPage - 1].classList.add('pagination-active');
+}
+
+
 // Get the elements
 const decreaseBtn = document.querySelector('.decrease-btn');
 const increaseBtn = document.querySelector('.increase-btn');
@@ -203,43 +318,6 @@ function filterBtn() {
 
 
 
-// // range********************
-// window.onload = function () {
-//     slideOne();
-//     slideTwo();
-// }
-
-// let sliderOne = document.getElementById("slider-1");
-// let sliderTwo = document.getElementById("slider-2");
-// let displayValOne = document.getElementById("range1");
-// let displayValTwo = document.getElementById("range2");
-// let minGap = 0;
-// let sliderTrack = document.querySelector(".slider-track");
-// let sliderMaxValue = document.getElementById("slider-1").max;
-
-// function slideOne() {
-//     if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
-//         sliderOne.value = parseInt(sliderTwo.value) - minGap;
-//     }
-//     displayValOne.textContent = sliderOne.value;
-//     fillColor();
-// }
-// function slideTwo() {
-//     if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
-//         sliderTwo.value = parseInt(sliderOne.value) + minGap;
-//     }
-//     displayValTwo.textContent = sliderTwo.value;
-//     fillColor();
-// }
-// function fillColor() {
-//     percent1 = (sliderOne.value / sliderMaxValue) * 100;
-//     percent2 = (sliderTwo.value / sliderMaxValue) * 100;
-//     sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #AA8117 ${percent1}% , #AA8117 ${percent2}%, #dadae5 ${percent2}%)`;
-// }
-
-
-// pdp 
-
 // account page 
 function accountBtn() {
     document.querySelector("#mobileAccount").classList.toggle("d-none");
@@ -248,3 +326,11 @@ function closeAccountBtn() {
     document.querySelector("#mobileAccount").classList.remove("d-block");
     document.querySelector("#mobileAccount").classList.add("d-none");
 }
+
+
+
+
+
+
+
+
