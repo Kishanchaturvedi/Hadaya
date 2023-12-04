66174eef3e1517069047219204e0f8e609420b6b
anurag4499 <kisan12chaturvedi@gmail.com>
2023-12-04 11:25:04 +0530

    seven-Commit

---
+++ assets/js/owl-carousel.js
@@ -0,0 +1,169 @@
+
+$('#carouselCard-2').owlCarousel({
+    loop: true,
+
+    // slideTransition: 'linear',
+    // responsiveClass:true,
+    items: 4,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            margin: 10,
+            items: 2
+        },
+        600: {
+            margin: 30,
+            items: 3
+        },
+        1200: {
+            margin: 50,
+            items: 5
+        }
+    }
+})
+
+$(document).ready(function () {
+    $('#carouselCard-1').owlCarousel({
+        loop: true,
+
+        margin: 15,
+        dots: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                autoWidth: true,
+                items: 2.5
+            }
+        }
+    })
+    $(".owl-prev").html('<i class="bi bi-arrow-left"></i>');
+    $(".owl-next").html('<i class="bi bi-arrow-right"></i>');
+});
+
+
+
+
+$(document).ready(function () {
+    $('#carousel-perspective').owlCarousel({
+        // loop: true,
+        margin: 15,
+        rtl: true,
+        loop: true,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 3000,
+        autoplaySpeed: 3000,
+        autoplayHoverPause: false,
+        autoWidth: true,
+        dots: false,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+    $(".owl-prev").html('<i class="bi bi-arrow-left"></i>');
+    $(".owl-next").html('<i class="bi bi-arrow-right"></i>');
+});
+
+
+$('#owl-carousel4').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1,
+            dots: true,
+        }
+    }
+})
+
+
+// pdp carousel 
+$(document).ready(function () {
+    $('#carouselCard-3').owlCarousel({
+        loop: true,
+        dots: false,
+        autoWidth: true,
+        nav: true,
+        responsive: {
+            0: {
+                margin: 10,
+                items: 1
+            },
+            600: {
+                margin: 15,
+                items: 3
+            },
+            1000: {
+                margin: 19,
+                items: 4
+            }
+        }
+    });
+    $(".owl-prev").html('<i class="bi bi-arrow-left"></i>');
+    $(".owl-next").html('<i class="bi bi-arrow-right"></i>');
+
+});
+// pdp carousel *************************************
+$('#owls').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    items: 4,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
+
+//********************* */ plp ***************************************************
+
+
+$('#paginationContainer').owlCarousel({
+    autoWidth: true,
+    margin: 15,
+    loop: false,
+    dots: false,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
 
 
-
-
-
-
 function openBtn() {
     document.getElementById("my-slider").style.display = "block"
 }
@@ -11,186 +7,6 @@ function closeBtn() {
     document.getElementById("my-slider").style.display = "none"
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-$('#carouselCard-2').owlCarousel({
-    loop: true,
-
-    // slideTransition: 'linear',
-    // responsiveClass:true,
-    items: 4,
-    nav: false,
-    dots: false,
-    responsive: {
-        0: {
-            margin: 10,
-            items: 2
-        },
-        600: {
-            margin: 30,
-            items: 3
-        },
-        1200: {
-            margin: 50,
-            items: 5
-        }
-    }
-})
-
-$(document).ready(function () {
-    $('#carouselCard-1').owlCarousel({
-        loop: true,
-
-        margin: 15,
-        dots: true,
-        nav: true,
-        responsive: {
-            0: {
-                items: 2
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                autoWidth: true,
-                items: 2.5
-            }
-        }
-    })
-    $(".owl-prev").html('<i class="bi bi-arrow-left"></i>');
-    $(".owl-next").html('<i class="bi bi-arrow-right"></i>');
-});
-
-
-
-
-$(document).ready(function () {
-    $('#carousel-perspective').owlCarousel({
-        // loop: true,
-        margin: 15,
-        rtl: true,
-        loop: true,
-        autoplay: true,
-        slideTransition: 'linear',
-        autoplayTimeout: 3000,
-        autoplaySpeed: 3000,
-        autoplayHoverPause: false,
-        autoWidth: true,
-        dots: false,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 4
-            }
-        }
-    })
-    $(".owl-prev").html('<i class="bi bi-arrow-left"></i>');
-    $(".owl-next").html('<i class="bi bi-arrow-right"></i>');
-});
-
-
-$('#owl-carousel4').owlCarousel({
-    loop: true,
-    margin: 20,
-    nav: false,
-    dots: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1,
-            dots: true,
-        }
-    }
-})
-
-
-// pdp carousel 
-$(document).ready(function () {
-    $('#carouselCard-3').owlCarousel({
-        loop: true,
-        dots: false,
-        autoWidth: true,
-        nav: true,
-        responsive: {
-            0: {
-                margin: 10,
-                items: 1
-            },
-            600: {
-                margin: 15,
-                items: 3
-            },
-            1000: {
-                margin: 19,
-                items: 4
-            }
-        }
-    });
-    $(".owl-prev").html('<i class="bi bi-arrow-left"></i>');
-    $(".owl-next").html('<i class="bi bi-arrow-right"></i>');
-
-});
-// pdp carousel *************************************
-$('#owls').owlCarousel({
-    loop: false,
-    margin: 10,
-    nav: false,
-    items: 4,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 2
-        }
-    }
-})
-
-//********************* */ plp ***************************************************
-
-
-$('#paginationContainer').owlCarousel({
-    autoWidth: true,
-    margin: 15,
-    loop: false,
-    dots: false,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 4
-        }
-    }
-})
 var acc = document.getElementsByClassName("accordions");
 var i;
 
@@ -243,6 +59,34 @@ function fillColor() {
 
 
 
+
+// pdp increse decress start ***********************************
+// Get the elements
+const decreaseBtn = document.querySelector('.decrease-btn');
+const increaseBtn = document.querySelector('.increase-btn');
+const countingSpan = document.querySelector('.counting');
+
+// Add event listeners to the buttons
+decreaseBtn.addEventListener('click', () => {
+    // Decrease the count by 1 when the decrease button is clicked
+    let count = parseInt(countingSpan.textContent);
+    if (count > 1) {
+        count--;
+        countingSpan.textContent = count;
+    }
+});
+
+increaseBtn.addEventListener('click', () => {
+    // Increase the count by 1 when the increase button is clicked
+    let count = parseInt(countingSpan.textContent);
+    count++;
+    countingSpan.textContent = count;
+});
+// pdp increse decress close ***********************************
+
+
+
+
 // pagination 
 function toggleActive(link) {
     var links = document.querySelectorAll('.nav-link.pagination a');
@@ -275,29 +119,6 @@ function updatePagination(currentPage) {
     var links = document.querySelectorAll('.nav-link.pagination a');
     links[currentPage - 1].classList.add('pagination-active');
 }
-
-
-// Get the elements
-const decreaseBtn = document.querySelector('.decrease-btn');
-const increaseBtn = document.querySelector('.increase-btn');
-const countingSpan = document.querySelector('.counting');
-
-// Add event listeners to the buttons
-decreaseBtn.addEventListener('click', () => {
-    // Decrease the count by 1 when the decrease button is clicked
-    let count = parseInt(countingSpan.textContent);
-    if (count > 1) {
-        count--;
-        countingSpan.textContent = count;
-    }
-});
-
-increaseBtn.addEventListener('click', () => {
-    // Increase the count by 1 when the increase button is clicked
-    let count = parseInt(countingSpan.textContent);
-    count++;
-    countingSpan.textContent = count;
-});
 
 
 // plp page************************
